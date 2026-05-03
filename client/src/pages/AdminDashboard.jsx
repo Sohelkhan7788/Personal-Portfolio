0702e2d17369b2ae09a2aca6bf9af421dbf753bc
Sohelkhan7788 <soyalkhan1402@gmail.com>
2026-05-03 15:53:13 +0530

    fix api remove axios

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { HiFolder, HiBadgeCheck, HiCode, HiMail, HiTrendingUp } from 'react-icons/hi';
-import api from '../api';
+import axios from 'axios';
 
 const StatCard = ({ title, value, icon, color, path, delay }) => (
   <motion.div
